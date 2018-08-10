f1ac1e09beea7b5414e9a6bd59a05e2cdd65fd4e
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2018-08-10 14:49:07 +0200

    shortening test_log2.c

--- tests/test_log2.c
+++ tests/test_log2.c
@@ -7,19 +7,22 @@
 
 int main() {
     int nonzero = 0;
-    ASSERT_EQUAL_INT_DECLARATIVE(&nonzero, log2i((unsigned char) 0), 0);
-    ASSERT_EQUAL_INT_DECLARATIVE(&nonzero, log2i(1), 0);
-    ASSERT_EQUAL_INT_DECLARATIVE(&nonzero, log2i(2), 1);
-    ASSERT_EQUAL_INT_DECLARATIVE(&nonzero, log2i(3), 1);
-    ASSERT_EQUAL_INT_DECLARATIVE(&nonzero, log2i(4), 2);
-    ASSERT_EQUAL_INT_DECLARATIVE(&nonzero, log2i(5), 2);
-    ASSERT_EQUAL_INT_DECLARATIVE(&nonzero, log2i((unsigned char) 255), 7);
-    ASSERT_EQUAL_INT_DECLARATIVE(&nonzero, log2c((unsigned char) 0), 0);
-    ASSERT_EQUAL_INT_DECLARATIVE(&nonzero, log2c(1), 1);
-    ASSERT_EQUAL_INT_DECLARATIVE(&nonzero, log2c(2), 1);
-    ASSERT_EQUAL_INT_DECLARATIVE(&nonzero, log2c(3), 2);
-    ASSERT_EQUAL_INT_DECLARATIVE(&nonzero, log2c(4), 2);
-    ASSERT_EQUAL_INT_DECLARATIVE(&nonzero, log2c(5), 2);
-    ASSERT_EQUAL_INT_DECLARATIVE(&nonzero, log2c((unsigned char) 255), 8);
+#define TEST_FUN(fun,x,y) \
+    ASSERT_EQUAL_INT_DECLARATIVE(&nonzero, ( ( fun ) ( x ) ) , ( y ) )
+#define TEST_UCHAR_FUN(fun,x,y) TEST_FUN(fun, ((unsigned char) ( x ) ), y)
+    TEST_UCHAR_FUN(log2i, 0, 0);
+    TEST_UCHAR_FUN(log2i, 1, 0);
+    TEST_UCHAR_FUN(log2i, 2, 1);
+    TEST_UCHAR_FUN(log2i, 3, 1);
+    TEST_UCHAR_FUN(log2i, 4, 2);
+    TEST_UCHAR_FUN(log2i, 5, 2);
+    TEST_UCHAR_FUN(log2i, 255, 7);
+    TEST_UCHAR_FUN(log2c, 0, 0);
+    TEST_UCHAR_FUN(log2c, 1, 1);
+    TEST_UCHAR_FUN(log2c, 2, 1);
+    TEST_UCHAR_FUN(log2c, 3, 2);
+    TEST_UCHAR_FUN(log2c, 4, 2);
+    TEST_UCHAR_FUN(log2c, 5, 2);
+    TEST_UCHAR_FUN(log2c, 255, 8);
     return nonzero;
 }
