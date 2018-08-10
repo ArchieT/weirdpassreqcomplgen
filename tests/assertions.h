e41d2fbb52494fc431be8dbc3fae37508fa85346
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2018-08-10 12:43:01 +0200

    assert_equal_int

--- tests/assertions.h
+++ tests/assertions.h
@@ -9,16 +9,11 @@
 #include "stdarg.h"
 #include "string.h"
 
-#define DEF_ASSERT_EQUAL ( ttype ) \
-void assert_equal_ ## ttype (int *nonzero, char* fstr, (ttype) a, (ttype) b) {\
-    if (*nonzero == 0 && a != b) {\
-        fprintf(stderr, fstr, a, b);\
-        *nonzero=8;\
-    }\
+void assert_equal_int(int *nonzero, char *fstr, int a, int b) {
+    if (*nonzero == 0 && a != b) {
+        fprintf(stderr, fstr, a, b);
+        *nonzero = 8;
+    }
 }
-
-DEF_ASSERT_EQUAL(int)
-
-DEF_ASSERT_EQUAL(char)
 
 #endif //WEIRDPASSREQCOMPLGEN_ASSERTIONS_H
