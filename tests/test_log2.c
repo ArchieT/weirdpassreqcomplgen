531fb94aaace51c8fc1351d480b5d12402fc4615
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2018-08-10 14:54:24 +0200

    temporary single letter macro shortening in test_log2

--- tests/test_log2.c
+++ tests/test_log2.c
@@ -10,19 +10,22 @@ int main() {
 #define TEST_FUN(fun,x,y) \
     ASSERT_EQUAL_INT_DECLARATIVE(&nonzero, ( ( fun ) ( x ) ) , ( y ) )
 #define TEST_UCHAR_FUN(fun,x,y) TEST_FUN(fun, ((unsigned char) ( x ) ), y)
-    TEST_UCHAR_FUN(log2i, 0, 0);
-    TEST_UCHAR_FUN(log2i, 1, 0);
-    TEST_UCHAR_FUN(log2i, 2, 1);
-    TEST_UCHAR_FUN(log2i, 3, 1);
-    TEST_UCHAR_FUN(log2i, 4, 2);
-    TEST_UCHAR_FUN(log2i, 5, 2);
-    TEST_UCHAR_FUN(log2i, 255, 7);
-    TEST_UCHAR_FUN(log2c, 0, 0);
-    TEST_UCHAR_FUN(log2c, 1, 1);
-    TEST_UCHAR_FUN(log2c, 2, 1);
-    TEST_UCHAR_FUN(log2c, 3, 2);
-    TEST_UCHAR_FUN(log2c, 4, 2);
-    TEST_UCHAR_FUN(log2c, 5, 2);
-    TEST_UCHAR_FUN(log2c, 255, 8);
+#define T(x,y) TEST_UCHAR_FUN(log2i,x,y)
+    T(0, 0);
+    T(1, 0);
+    T(2, 1);
+    T(3, 1);
+    T(4, 2);
+    T(5, 2);
+    T(255, 7);
+#undef T
+#define T(x,y) TEST_UCHAR_FUN(log2c,x,y)
+    T(0, 0);
+    T(1, 1);
+    T(2, 1);
+    T(3, 2);
+    T(4, 2);
+    T(5, 2);
+    T(255, 8);
     return nonzero;
 }
