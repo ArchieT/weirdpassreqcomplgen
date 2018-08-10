c58d1aa66f055c8b65bb0b7881e24c7aca4709cc
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2018-08-10 14:31:37 +0200

    test_log2 unsigned char unsigned long long

--- tests/test_log2.c
+++ tests/test_log2.c
@@ -7,19 +7,19 @@
 
 int main() {
     int nonzero = 0;
-    ASSERT_EQUAL_INT_DECLARATIVE(&nonzero, log2i(0), 0);
+    ASSERT_EQUAL_INT_DECLARATIVE(&nonzero, log2i((unsigned char) 0), 0);
     ASSERT_EQUAL_INT_DECLARATIVE(&nonzero, log2i(1), 0);
     ASSERT_EQUAL_INT_DECLARATIVE(&nonzero, log2i(2), 1);
     ASSERT_EQUAL_INT_DECLARATIVE(&nonzero, log2i(3), 1);
     ASSERT_EQUAL_INT_DECLARATIVE(&nonzero, log2i(4), 2);
     ASSERT_EQUAL_INT_DECLARATIVE(&nonzero, log2i(5), 2);
-    ASSERT_EQUAL_INT_DECLARATIVE(&nonzero, log2i(255), 7);
-    ASSERT_EQUAL_INT_DECLARATIVE(&nonzero, log2c(0), 0);
+    ASSERT_EQUAL_INT_DECLARATIVE(&nonzero, log2i((unsigned char) 255), 7);
+    ASSERT_EQUAL_INT_DECLARATIVE(&nonzero, log2c((unsigned char) 0), 0);
     ASSERT_EQUAL_INT_DECLARATIVE(&nonzero, log2c(1), 1);
     ASSERT_EQUAL_INT_DECLARATIVE(&nonzero, log2c(2), 1);
     ASSERT_EQUAL_INT_DECLARATIVE(&nonzero, log2c(3), 2);
     ASSERT_EQUAL_INT_DECLARATIVE(&nonzero, log2c(4), 2);
     ASSERT_EQUAL_INT_DECLARATIVE(&nonzero, log2c(5), 2);
-    ASSERT_EQUAL_INT_DECLARATIVE(&nonzero, log2c(255), 8);
+    ASSERT_EQUAL_INT_DECLARATIVE(&nonzero, log2c((unsigned char) 255), 8);
     return nonzero;
 }
