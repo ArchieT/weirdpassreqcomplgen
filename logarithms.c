abfa6d617a19269b552d0b1524758716f0f75b37
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2018-08-14 10:27:57 +0200

    discontinued.

--- logarithms.c
+++ logarithms.c
@@ -9,6 +9,12 @@ signed char log2i(unsigned long long n) {
     return r;
 }
 
+signed char log8i(unsigned long long n) {
+    signed char r = 0;
+    while (n >>= 3) ++r;
+    return r;
+}
+
 signed char log2c(unsigned long long n) {
     n++;
     signed char r;
@@ -19,4 +25,28 @@ signed char log2c(unsigned long long n) {
         r++;
     }
     return r;
+}
+
+signed char log8c(unsigned long long n) {
+    n++;
+    signed char r;
+    if (n != 0) {
+        r = log8i(n);
+    } else {
+        r = log8i(--n);
+        r++;
+    }
+    return r;
+}
+
+size_t bytes_for_base(unsigned char b, unsigned long long n) {
+    /* let x = n^b, i.e. such that n = log_b(x), precisely
+     * let r be the least integer such that x ≤ 8^r
+     * r would have to be ceiling of log8(x)
+     * /log8(x)\ ≈ /log2(x)\ ~/~ \log2(8)/
+     * log2(x) = log_b(x) ~*~ log2(b)
+     * log_b(2) = 1 ~/~ log2(b)
+     * log_b(x) = log2(x) ~/~ log2(b)
+     *   */
+    return 0;
 }
--- logarithms.h
+++ logarithms.h
@@ -5,7 +5,14 @@
 #ifndef WEIRDPASSREQCOMPLGEN_LOG2_H
 #define WEIRDPASSREQCOMPLGEN_LOG2_H
 
+#include <string.h>
+
 signed char log2i(unsigned long long n);
 signed char log2c(unsigned long long n);
+
+signed char log8i(unsigned long long n);
+signed char log8c(unsigned long long n);
+
+size_t bytes_for_base(unsigned char b);
 
 #endif //WEIRDPASSREQCOMPLGEN_LOG2_H
